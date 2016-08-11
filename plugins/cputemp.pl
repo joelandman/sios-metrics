f6eb514c60c08281af58c5bde08a6a439022e90b
joelandman <joe.landman@gmail.com>
2016-08-11 14:42:30 -0400

    Rewrote cputemp.pl to be sane

--- plugins/cputemp.pl
+++ plugins/cputemp.pl
@@ -13,44 +13,60 @@ $| = 1;
 
 
 my ($path,$fpath,$t,$ifh,$fn,$data,$read,$l,$cpu_index,$hostname);
-my ($d,$de,$ce,%dir,%coretemp,$name,$ee,$temp,$core,$p,$fl);
+my ($d,$de,$ce,%dir,%coretemp,$name,$ee,$temp,$core,$p,$fl,@cpu,$e);
 $path   = '/sys/devices/platform';
+
+# process:
+#  1) Generate a hash of all the file names, socket numbers, and core indexes
+#  2) loop over this hash to read what we need to return the actual temp
+
 chomp($hostname = `hostname`);
 my ($uname,$krev);
 chomp($uname = `uname -r`);
 $krev = $1 if ($uname =~ /(\d\.\d+)\..*?/);
 
-while (1) {
-    printf "\n#### sync:%i\n",time;
-    tie %dir, 'IO::Dir', $path;
-    foreach $de (sort keys %dir) {
-        next if ($de !~ /coretemp/); #skip anything not coretemp relateda
- 	if ($krev < 3.18) {
-	        $fpath = join('/',$path,$de);
-	   }
-	  else
-	   {
-		$fpath = join('/',$path,$de,'hwmon/hwmon2');
-	   }
-        tie %coretemp, 'IO::Dir', $fpath;
-        if ($de =~ /coretemp\.(\d+)/) { $cpu_index = $1; }
-        foreach $ce (sort keys %coretemp) {
-            next if ($ce !~ /label$/);
-            $fl = sprintf('%s/%s',$fpath,$ce);
-            $p = &_get_contents($fl);
-            next if ($p =~ /Physical\sid/); # skip package
-            if ($p =~ /Core\s(\d+)/) {
+tie %dir, 'IO::Dir', $path;
+# only use the coretemp.\d+ entries
+foreach $de (sort keys %dir) {
+  next if ($de !~ /coretemp/); #skip anything not coretemp related
+  $cpu_index = $1 if ($de =~ /coretemp\.(\d+)/);
+  $fpath = (
+            $krev < 3.18 ?
+            join('/',$path,$de) :
+            join('/',$path,$de,(sprintf 'hwmon/hwmon%i',$cpu_index+1))
+           );
+  # yeah, that whole $cpu_index + 1 thing is because the /sys/devices/platform
+  # heirarchy is not as predictably named as one might like
+  tie %coretemp, 'IO::Dir', $fpath;
+  foreach $ce (sort keys %coretemp) {
+    next if ($ce !~ /label$/);
+    $fl = sprintf('%s/%s',$fpath,$ce);
+    $p = &_get_contents($fl);
+    next if ($p =~ /Physical\sid/);
+    if ($p =~ /Core\s(\d+)/) {
               $core = $1;
               $fn = $fl;
               $fn =~ s/label$/input/;
-              #$fn = sprintf('%s/temp%s_input',$fpath,$core);
-              $temp = &_get_contents($fn);
-              printf "cputemp,core=%i,machine=%s,socket=%i coretemp=%.1f\n",$core,$hostname,$cpu_index,$temp/1000.0;
-            }            
-        }
-        untie %coretemp;
+              push @cpu,{'file' => $fn, 'socket' => $cpu_index, 'core' => $core};
     }
-    untie %dir;
+  }
+  untie %coretemp;
+}
+untie %dir;
+
+
+while (1) {
+    printf "\n#### sync:%i\n",time;
+    # loop over all the elements in the @cpu array, reading file contents as
+    # needed
+    foreach $e (@cpu) {
+      $temp = &_get_contents($e->{'file'});
+      printf "cputemp,core=%i,machine=%s,socket=%i coretemp=%.1f\n",
+        $e->{'core'},
+        $hostname,
+        $e->{'socket'},
+        $temp/1000.0;
+    }
     sleep(1);
 }
 
